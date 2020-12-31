d8f16bc23bf71031567ea1dc148bceb64efa5139
Raul <rtf8519@gmail.com>
2020-12-30 22:13:55 -0500

    edit mongoose connection

--- server.js
+++ server.js
@@ -15,14 +15,21 @@ app.use(express.json());
 
 app.use(express.static("public"));
 
-mongoose.connect("mongodb://localhost/budget", {
-  useNewUrlParser: true,
-  useFindAndModify: false
+mongoose.connect(process.env.MONGODB_URI ||"mongodb://localhost/budget", {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  useCreateIndex: true,
+  useFindAndModify: false
 });
 
 // routes
 app.use(require("./routes/api.js"));
 
+app.get("/budget", (res, req) => {
+  res.sendFile(path.join(__dirname, "public/index.html"));
+});
+
+
 app.listen(PORT, () => {
-  console.log(`App running on port ${PORT}!`);
+  console.log(`App running on port ${PORT}!`);
 });
